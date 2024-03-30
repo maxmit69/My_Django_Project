fc0758d335ad9b6938409df9386512ad78aa0404
Aristov Sergei <maxmit83@gmail.com>
2024-03-30 10:28:04 +0500

    Выполнение задания №5 из домашней работы

--- catalog/models.py
+++ catalog/models.py
@@ -5,15 +5,15 @@ NULLABLE = {'null': True, 'blank': True}
 
 class Product(models.Model):
     name_prod = models.CharField(max_length=150, verbose_name="наименование продукта")
-    description = models.TextField(verbose_name="описание")
+    description_prod = models.TextField(verbose_name="описание")
     preview = models.ImageField(upload_to='product/', verbose_name="превью", **NULLABLE)
     purchase_price = models.IntegerField(verbose_name="цена за покупку")
     date_created = models.DateTimeField(auto_now_add=True, verbose_name="дата создания")
     date_updated = models.DateTimeField(auto_now=True, verbose_name="дата последнего изменения")
-    category = models.ForeignKey('Category', on_delete=models.PROTECT)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="категория")
 
     def __str__(self):
-        return f'{self.name_prod} {self.category} {self.description}'
+        return f'{self.name_prod} {self.category} {self.description_prod}'
 
     class Meta:
         verbose_name = "продукт"
@@ -22,10 +22,10 @@ class Product(models.Model):
 
 class Category(models.Model):
     name_cat = models.CharField(max_length=150, verbose_name="наименование категории")
-    description = models.TextField(verbose_name="описание")
+    description_cat = models.TextField(verbose_name="описание")
 
     def __str__(self):
-        return f'{self.name_cat} {self.description}'
+        return f'{self.name_cat}'
 
     class Meta:
         verbose_name = "категория"
